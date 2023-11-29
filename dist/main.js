d48a657eebb36b268e9b9fe00b7ee9762e167195
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-29 12:19:51 -0500

    Vid379[seccion27]-Desplegar backend a Railway

--- dist/main.js
+++ dist/main.js
@@ -5,11 +5,12 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
         forbidNonWhitelisted: true,
     }));
-    await app.listen(3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
+app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,7 +13,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 
 }
 bootstrap();
